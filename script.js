a9824f839b325bc2ba7f1bf0716861a222f3cdbc
Bgomez718 <bgomez718@gmail.com>
2019-08-03 12:06:26 -0400

    js add on

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("#mySidepanel").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0px";
+    document.getElementById("#mySidepanel").style.width = "0px";
 }
